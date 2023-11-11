8686eb1d846f35676a20e945c968b7d03bb84463
rahulsh468 <rahul@avellinoconsulting.com>
2023-11-11 14:02:11 +0530

    clustering manual

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+process.env.UV_THREADPOOL_SIZE = 1;
 const cluster = require('cluster');
 const express = require("express");
+const crypto = require('crypto');
 const app = express();
 
 // is the file in mastreMode
@@ -8,15 +10,12 @@ if (cluster.isMaster) {
     cluster.fork();
 } else {
     // thill act like a child and only run as server and nthg else
-    function doWork(duration) {
-        const start = Date.now();
-        while (Date.now() - start < duration) {
-        }
-    }
-
     app.get('/', (req, res) => {
-        doWork(5000)
-        res.send("Hello");
+        const start = Date.now();
+        crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+            console.log('1:', Date.now() - start)
+            res.send("Hello");
+        })
     })
 
     app.get('/fast', (req, res) => {
